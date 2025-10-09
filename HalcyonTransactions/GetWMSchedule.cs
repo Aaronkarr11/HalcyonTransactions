7065196effe0a4948a789c00dfe2ab2d57757319
Aaron Karr <aaron.karr11@gmail.com>
2025-10-08 21:21:57 -0700

    fixed GetWMSchedule

--- HalcyonTransactions/GetWMSchedule.cs
+++ HalcyonTransactions/GetWMSchedule.cs
@@ -7,6 +7,8 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Microsoft.Azure.Functions.Worker;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Globalization;
+using System.Collections.Concurrent;
 
 namespace HalcyonTransactions
 {
@@ -31,14 +33,14 @@ namespace HalcyonTransactions
 
                 TableClient client = new TableClient(conString, $"{RequestObject.DeviceName}WMSchedule");
                 await client.CreateIfNotExistsAsync();
-                Pageable<WMScheduleTableEntity> entities = client.Query<WMScheduleTableEntity>();
+                WMScheduleTableEntity record = client.GetEntityAsync<WMScheduleTableEntity>(RequestObject.PartitionKey, RequestObject.RowKey).Result;
 
                 WMScheduleModel model = new WMScheduleModel();
 
-                model.RowKey = RequestObject.RowKey;
-                model.PartitionKey = RequestObject.PartitionKey;
-                model.DeviceName = RequestObject.DeviceName;
-                model.StartingDate = RequestObject.StartingDate;
+                model.RowKey = record.RowKey;
+                model.PartitionKey = record.PartitionKey;
+                model.DeviceName = record.DeviceName;
+                model.StartingDate = Convert.ToDateTime(record.StartingDate).ToString("MM/dd/yyyy", CultureInfo.InvariantCulture);
 
                 return new OkObjectResult(JsonConvert.SerializeObject(model));
             }
